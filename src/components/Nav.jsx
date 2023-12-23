236d79746001df659233e338b9b7a1f98a16f424
Aryan Jain <aryan.jain2021@vitbhopal.ac.in>
2023-12-23 15:43:34 +0530

    now the scroll effect is added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,38 @@
-import { Link } from "react-router-dom"
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Nav() {
+    const location = useLocation();
+
+    useEffect(() => {
+        // Scroll to the top when the route changes
+        window.scrollTo(0, 0);
+    }, [location.pathname]);
+
     return (
         <div className="navbar sticky">
-
             <ul className="navListbar">
-                <li className="navList"><Link className="navbar" to="/">Home</Link></li>
-                <li className="navList"><Link className="navbar" to="/about">About</Link></li>
-                <li className="navList"><Link className="navbar" to="/projects">Projects</Link></li>
-                <li className="navList"><Link className="navbar" to="/contact">Contact</Link></li>
+                <li className="navList">
+                    <Link className="navbar" to="/">
+                        Home
+                    </Link>
+                </li>
+                <li className="navList">
+                    <Link className="navbar" to="/about">
+                        About
+                    </Link>
+                </li>
+                <li className="navList">
+                    <Link className="navbar" to="/projects">
+                        Projects
+                    </Link>
+                </li>
+                <li className="navList">
+                    <Link className="navbar" to="/contact">
+                        Contact
+                    </Link>
+                </li>
             </ul>
-
         </div>
-    )
+    );
 }
-
