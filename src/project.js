0510e808db4f73eacb7dfa731043b644b23e2d0d
Aryan Jain <aryan.jain2021@vitbhopal.ac.in>
2024-05-07 11:01:26 +0530

    image updates

--- src/project.js
+++ src/project.js
@@ -1,7 +1,7 @@
 const project = [
     {
         "id": '1',
-        "img": "https://cdn.discordapp.com/attachments/1059849186884985004/1181994924267290716/UB.png?ex=662c7e4e&is=662b2cce&hm=3f7187bd62c8e4fff97d12771b4572406cd6d5bbb2feb76e767ea99dc2e15925&",
+        "img": "https://i.ibb.co/NnWqTnz/UB.png",
         "proj_title": "UNDERBelly",
         "description": "Food Order Website for the college food Court",
         "link": "https://github.com/trojan-kins/UnderBelly-Website",
@@ -15,14 +15,14 @@ const project = [
     // },
     {
         "id": '3',
-        "img": "https://cdn.discordapp.com/attachments/1059849186884985004/1182331335386349629/ada93729daf922ad0318c8c0295e5cb477921808-1456x816.webp?ex=662c661d&is=662b149d&hm=df8fbd33e8806557de8b4126e0aac871348782faa3605816850173fcae66cdd9&",
+        "img": "https://i.ibb.co/ThQNPvj/ML.webp",
         "proj_title": "Traffic Prediction",
         "description": "Machine learning Model using GRU. Technologies used: Python, GoogleCollab, Anaconda.",
         "link": "https://colab.research.google.com/drive/15Af3StHGLK9f1ThfJ4Es4LwYHaEVDsg3?usp=sharing",
     },
     {
         "id": '4',
-        "img": "https://cdn.discordapp.com/attachments/1059849186884985004/1214914900648919060/suraksha.png?ex=662c48ec&is=662af76c&hm=8b91550278abc97d038f1a96b2462e1fb2d6b3e50ab133ca6f5bf7525fc2f972&",
+        "img": "https://i.ibb.co/Y8VJM3N/suraksha.png",
         "proj_title": "Suraksha Setu",
         "description": "React App on Disaster Management",
         "link": "https://suraksha-setu-vy8h.vercel.app/",
